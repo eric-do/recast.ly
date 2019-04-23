cd0d4c012498fb4c131e447c912e8add68b702f6
eric-do <ericdo.617@gmail.com>
2019-04-22 21:18:48 -0700

    Attempt at youtube API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
+import '../config/youtube.js';
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  var query = options.query;
+  var max = options.max || 5;
+  var key = options.key;
+  
+  $.ajax({
+    cache: false,
+    data: $.extend({
+      key: YOUTUBE_API_KEY,
+      q: query,
+      part: 'snippet'
+    }, { maxResults: max,
+      dataType: 'json',
+      type: 'GET',
+      timeout: 5000,
+      url: 'https://www.googleapis.com/youtube/v3/search'
+    })
+  });
 };
 
 export default searchYouTube;
