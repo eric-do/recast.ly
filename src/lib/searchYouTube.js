8ee73c358fad51ab0bc3cee953292906273db596
eric-do <ericdo.617@gmail.com>
2019-04-23 19:21:46 -0700

    Refactored to use ES6 default values for objects

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,6 +1,6 @@
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
-var searchYouTube = (options, callback) => {
+var searchYouTube = ({ query = 'test', max = 5, key = YOUTUBE_API_KEY }, callback) => {
   // Input: an options object containing parameters for the YouTube API search (query, max, key)
   // This function makes an ajax call to the Google API, and receives a JSON object
   // Since this project requires the object match data structure with the example data structure, 
@@ -8,9 +8,6 @@ var searchYouTube = (options, callback) => {
   // in the object where the video array is located (data.items)
   // Upon successful pulling of the data, this function will fire the callback function (success callback)
   // The success callback should call this.setState, so that App will rerender with the new data
-  var query = options ? options.query : 'test';
-  var max = options ? options.max || 5 : 5;
-  var key = options ? options.key || YOUTUBE_API_KEY : YOUTUBE_API_KEY;
   
   $.ajax({
     url: `https://www.googleapis.com/youtube/v3/search`,
